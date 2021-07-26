d2e059bf64a5ea4a97605dea1ecd9cb2de0dde4c
emanuelhardwell <emanuelnoe16@gmail.com>
2021-07-26 10:59:26 -0500

    Se actualizo el objeto infiormacion

--- src/components/SelectScreen.js
+++ src/components/SelectScreen.js
@@ -1,20 +1,48 @@
 import React, { useState } from "react";
+import Swal from "sweetalert2";
 
-const categorias = [
+const informacion = [
   {
-    nombre: "audio y video",
-    articulos: ["monitor led", "parlante bluetoo", "samrt tv"],
+    categorias: {
+      id: 1,
+      nombre: "preliminares",
+    },
+    articulos: [
+      { nombreArticulo: "castillo k armado con 4 varillas", unidad: "ml" },
+      { nombreArticulo: "castillo k2 armado con 4 varillas ", unidad: "ml" },
+      { nombreArticulo: "castillo k3 armado con 6 varillas", unidad: "ml" },
+      { nombreArticulo: "columna de 30cm de diametro", unidad: "ml" },
+      { nombreArticulo: "muro de tabicon de 14cm de espesor", unidad: "m2" },
+      { nombreArticulo: "dala armada con 4 varillas", unidad: "ml" },
+      {
+        nombreArticulo: "cadena de cerramiento armada con 6 varillas",
+        unidad: "ml",
+      },
+      { nombreArticulo: "trabe armada armada con 4 varillas", unidad: "ml" },
+      { nombreArticulo: "losa de entrepiso concreto armado", unidad: "ml" },
+      { nombreArticulo: "otro", unidad: "m2" },
+    ],
   },
   {
-    nombre: "ferreteria",
-    articulos: ["martillo pequeño", "taladro percusión", "serrucho corte fino"],
-  },
-  {
-    nombre: "verdura",
-    articulos: ["calabacitas", "cebolla", "ajo en polvo"],
+    categorias: {
+      id: 2,
+      nombre: "azotea",
+    },
+    articulos: [
+      { nombreArticulo: "dala armada con 4 varillas", unidad: "ml" },
+      { nombreArticulo: "castillo k armado con 4 varillas", unidad: "ml" },
+      { nombreArticulo: "muro de tabicon de 14cm", unidad: "m2" },
+      {
+        nombreArticulo: "cadena de cerramiento armada con 6 varillas",
+        unidad: "ml",
+      },
+      { nombreArticulo: "rotulo", unidad: "lote" },
+      { nombreArticulo: "nivelacion de losa de  azotea", unidad: "m2" },
+      { nombreArticulo: "otro", unidad: "ml" },
+    ],
   },
 ];
-// console.log(categorias);
+// console.log(informacion);
 
 export const SelectScreen = () => {
   const [articulos, setArticulos] = useState(-1);
@@ -35,8 +63,25 @@ export const SelectScreen = () => {
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    console.log(e.target.value);
+    let index = e.target.selectedIndex;
+    let dato = e.target.options[index].text;
+    let datoPro = e.target.options[index].getAttribute("data-key");
+    // console.log(e.target);
     setData({ ...data, [name]: value });
+  };
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+
+    if (categoria === "" || categoria === -1) {
+      return Swal.fire("Error", "Selecciona una categoria y articulo", "error");
+    }
+
+    if (articulo === "") {
+      return Swal.fire("Error", "Selecciona una categoria y articulo", "error");
+    }
+
+    console.log(data);
   };
 
   // return ********************************
@@ -44,49 +89,68 @@ export const SelectScreen = () => {
     <>
       <div className="row">
         <div className="col-md-6">
-          <h3> Categoria</h3>
-          <div className="form-floating">
-            <select
-              className="form-select"
-              id="floatingSelect"
-              aria-label="Floating label select example"
-              name="categoria"
-              value={categoria}
-              onChange={handleInputChange}
-              onClick={handleCargarArticulos}
-            >
-              <option value={-1}> Selecciona tu categoria </option>
-              {categorias.map((item, i) => (
-                <option key={i} value={i}>
-                  {item.nombre}
-                </option>
-              ))}
-            </select>
-            <label htmlFor="floatingSelect"> Categorias </label>
+          <div className="card">
+            <div className="card-body">
+              <form onSubmit={handleSubmitForm}>
+                <h3> Categoria</h3>
+                <div className="form-floating mb-2">
+                  <select
+                    className="form-select"
+                    id="floatingSelect"
+                    aria-label="Floating label select example"
+                    name="categoria"
+                    value={categoria}
+                    onChange={handleInputChange}
+                    onClick={handleCargarArticulos}
+                  >
+                    <option value={-1}> Selecciona tu categoria </option>
+                    {informacion.map((item, i) => (
+                      <option key={i} value={i} data-key={item.nombre}>
+                        {item.categorias.nombre}
+                      </option>
+                    ))}
+                  </select>
+                  <label htmlFor="floatingSelect"> Categorias </label>
+                </div>
+
+                <h3>Productos</h3>
+                <div className="form-floating mb-2">
+                  <select
+                    className="form-select"
+                    id="floatingSelect2"
+                    aria-label="Floating label select example"
+                    name="articulo"
+                    value={articulo}
+                    onChange={handleInputChange}
+                  >
+                    <option value="hola"> Selecciona tu articulo </option>
+                    {articulos > -1 &&
+                      informacion[articulos].articulos.map((item, i) => (
+                        <option
+                          key={i}
+                          value={item.nombreArticulo}
+                          data-key={item.nombreArticulo}
+                        >
+                          {item.nombreArticulo}
+                        </option>
+                      ))}
+                  </select>
+                  <label htmlFor="floatingSelect2"> Articulos </label>
+                </div>
+
+                <div className="d-grid gap-2">
+                  <button type="submit" className="btn btn-primary">
+                    <i className="far fa-save me-2"></i>
+                    <span> Agregar </span>
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
         {/* col-md-6 */}
         <div className="col-md-6">
-          <h3>Productos</h3>
-          <div className="form-floating">
-            <select
-              className="form-select"
-              id="floatingSelect2"
-              aria-label="Floating label select example"
-              name="articulo"
-              value={articulo}
-              onChange={handleInputChange}
-            >
-              <option value="hokla"> Selecciona tu articulo </option>
-              {articulos > -1 &&
-                categorias[articulos].articulos.map((item, i) => (
-                  <option key={i} value={i}>
-                    {item}
-                  </option>
-                ))}
-            </select>
-            <label htmlFor="floatingSelect2"> Articulos </label>
-          </div>
+          <h3> hola menu </h3>
         </div>
       </div>
     </>
