99fb46955d45637c9e300708fbeca5efe2b321d9
emanuelhardwell <emanuelnoe16@gmail.com>
2021-07-26 14:42:55 -0500

    Se agregaron mas objetos

--- src/components/SelectScreen.js
+++ src/components/SelectScreen.js
@@ -4,41 +4,111 @@ import Swal from "sweetalert2";
 const informacion = [
   {
     categorias: {
-      id: 1,
+      id: 0,
       nombre: "preliminares",
     },
     articulos: [
-      { nombreArticulo: "castillo k armado con 4 varillas", unidad: "ml" },
-      { nombreArticulo: "castillo k2 armado con 4 varillas ", unidad: "ml" },
-      { nombreArticulo: "castillo k3 armado con 6 varillas", unidad: "ml" },
-      { nombreArticulo: "columna de 30cm de diametro", unidad: "ml" },
-      { nombreArticulo: "muro de tabicon de 14cm de espesor", unidad: "m2" },
-      { nombreArticulo: "dala armada con 4 varillas", unidad: "ml" },
       {
-        nombreArticulo: "cadena de cerramiento armada con 6 varillas",
-        unidad: "ml",
+        nombre: "suministro y colocacion de letrero",
+        unidad: "pza",
+        precio: 3500,
       },
-      { nombreArticulo: "trabe armada armada con 4 varillas", unidad: "ml" },
-      { nombreArticulo: "losa de entrepiso concreto armado", unidad: "ml" },
-      { nombreArticulo: "otro", unidad: "m2" },
+      {
+        nombre: "trazo y nivelacion de terreno en estructuras",
+        unidad: "m2",
+        precio: 9,
+      },
+      { nombre: "otro", unidad: "pza", precio: 0 },
+    ],
+  },
+  {
+    categorias: {
+      id: 1,
+      nombre: "azotea",
+    },
+    articulos: [
+      { nombre: "excavacion a cielo abierto", unidad: "m3", precio: 212 },
+      { nombre: "retiro de material de excavacion", unidad: "m3", precio: 296 },
+      {
+        nombre: "plantilla de concreto hecho en obra",
+        unidad: "m2",
+        precio: 165,
+      },
+      { nombre: "zapata corrida de concreto", unidad: "m2", precio: 854 },
+      { nombre: "zapata aislada de concreto", unidad: "pza", precio: 1460 },
+      { nombre: "contratrabe de concreto", unidad: "ml", precio: 567 },
+      { nombre: "trabe de liga de concreto", unidad: "ml", precio: 567 },
+      { nombre: "dado armado con 8 varillas", unidad: "ml", precio: 2084 },
+      { nombre: "castillo k armado con 4 varillas", unidad: "ml", precio: 292 },
+      {
+        nombre: "castillo k2 armado con 4 varillas",
+        unidad: "ml",
+        precio: 405,
+      },
+      {
+        nombre: "castillo k3 armado con 6 varillas",
+        unidad: "ml",
+        precio: 479,
+      },
+      { nombre: "muro de enrase de tabicon", unidad: "m2", precio: 501 },
+      {
+        nombre: "cadena de desplante armada con 6 varillas",
+        unidad: "ml",
+        precio: 479,
+      },
+      {
+        nombre: "relleno con material producto de excavacion",
+        unidad: "m3",
+        precio: 155,
+      },
+      {
+        nombre: "suministro y rellenado con material",
+        unidad: "m3",
+        precio: 415,
+      },
+      { nombre: "otro", unidad: "pza", precio: 0 },
     ],
   },
   {
     categorias: {
       id: 2,
-      nombre: "azotea",
+      nombre: "estructura primer nivel",
     },
     articulos: [
-      { nombreArticulo: "dala armada con 4 varillas", unidad: "ml" },
-      { nombreArticulo: "castillo k armado con 4 varillas", unidad: "ml" },
-      { nombreArticulo: "muro de tabicon de 14cm", unidad: "m2" },
+      { nombre: "castillo k armado con 4 varillas", unidad: "ml", precio: 292 },
       {
-        nombreArticulo: "cadena de cerramiento armada con 6 varillas",
+        nombre: "castillo k2 armado con 4 varillas ",
         unidad: "ml",
+        precio: 405,
       },
-      { nombreArticulo: "rotulo", unidad: "lote" },
-      { nombreArticulo: "nivelacion de losa de  azotea", unidad: "m2" },
-      { nombreArticulo: "otro", unidad: "ml" },
+      {
+        nombre: "castillo k3 armado con 6 varillas",
+        unidad: "ml",
+        precio: 479,
+      },
+      { nombre: "columna de 30cm de diametro", unidad: "ml", precio: 918 },
+      {
+        nombre: "muro de tabicon de 14cm de espesor",
+        unidad: "m2",
+        precio: 401,
+      },
+      { nombre: "dala armada con 4 varillas", unidad: "ml", precio: 373 },
+      {
+        nombre: "cadena de cerramiento armada con 6 varillas",
+        unidad: "ml",
+        precio: 415,
+      },
+      {
+        nombre: "trabe armada armada con 4 varillas",
+        unidad: "ml",
+        precio: 453,
+      },
+      {
+        nombre: "losa de entrepiso concreto armado",
+        unidad: "ml",
+        precio: 844,
+      },
+      { nombre: "otro", unidad: "m2", precio: 0 },
     ],
   },
 ];
@@ -50,6 +120,9 @@ export const SelectScreen = () => {
   const initialState = {
     categoria: "",
     articulo: "",
+    descripcion: "",
+    cantidad: "",
+    total: "",
   };
 
   const handleCargarArticulos = (e) => {
@@ -59,13 +132,13 @@ export const SelectScreen = () => {
   };
 
   const [data, setData] = useState(initialState);
-  const { categoria, articulo } = data;
+  const { categoria, articulo, descripcion, cantidad, total } = data;
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    let index = e.target.selectedIndex;
-    let dato = e.target.options[index].text;
-    let datoPro = e.target.options[index].getAttribute("data-key");
+    // let index = e.target.selectedIndex;
+    // let dato = e.target.options[index].text;
+    // let datoPro = e.target.options[index].getAttribute("data-key");
     // console.log(e.target);
     setData({ ...data, [name]: value });
   };
@@ -105,7 +178,7 @@ export const SelectScreen = () => {
                   >
                     <option value={-1}> Selecciona tu categoria </option>
                     {informacion.map((item, i) => (
-                      <option key={i} value={i} data-key={item.nombre}>
+                      <option key={i} value={i}>
                         {item.categorias.nombre}
                       </option>
                     ))}
@@ -126,16 +199,54 @@ export const SelectScreen = () => {
                     <option value="hola"> Selecciona tu articulo </option>
                     {articulos > -1 &&
                       informacion[articulos].articulos.map((item, i) => (
-                        <option
-                          key={i}
-                          value={item.nombreArticulo}
-                          data-key={item.nombreArticulo}
-                        >
-                          {item.nombreArticulo}
+                        <option key={i} value={item.nombre}>
+                          {item.nombre}
                         </option>
                       ))}
                   </select>
                   <label htmlFor="floatingSelect2"> Articulos </label>
+                </div>
+
+                <h3> Descripción </h3>
+                <div className="form-floating mb-2">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="floatingInput3"
+                    placeholder="a"
+                    name="descripcion"
+                    value={descripcion}
+                    onChange={handleInputChange}
+                  />
+                  <label htmlFor="floatingInput3"> Descripción </label>
+                </div>
+
+                <h3> Cantidad </h3>
+                <div className="form-floating mb-2">
+                  <input
+                    type="number"
+                    className="form-control"
+                    id="floatingInput9"
+                    placeholder="a"
+                    name="cantidad"
+                    value={cantidad}
+                    onChange={handleInputChange}
+                  />
+                  <label htmlFor="floatingInput9"> Cantidad </label>
+                </div>
+
+                <h3> total </h3>
+                <div className="form-floating mb-2">
+                  <input
+                    type="number"
+                    className="form-control"
+                    id="floatingInput1"
+                    placeholder="a"
+                    name="total"
+                    value={total}
+                    onChange={handleInputChange}
+                  />
+                  <label htmlFor="floatingInput1"> Total </label>
                 </div>
 
                 <div className="d-grid gap-2">
